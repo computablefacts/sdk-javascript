fac603d3dde6483db6250cecd50ee57b50ef4831
Patrick Brisacier <pbrisacier@mncc.fr>
2021-08-03 23:52:18 +0200

    Add minified version.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,17 @@
+import {terser} from 'rollup-plugin-terser';
+
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/cf.js',
-    format: 'cjs'
-  }
+  output: [
+    {
+      file: 'dist/cf.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/cf.min.js',
+      format: 'iife',
+      name: 'version',
+      plugins: [terser()]
+    }
+  ]
 };
