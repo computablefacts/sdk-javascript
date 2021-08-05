0ac2f6c4c27095618380235ec18c08bb9a9eb584
Patrick Brisacier <pbrisacier@mncc.fr>
2021-08-05 15:55:31 +0200

    Refactor npm scripts.

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
 import {terser} from 'rollup-plugin-terser';
 
 export default {
-  input: 'src/index.ts',
+  input: 'dist/index.js',
   output: [
     {
-      file: 'dist/bundle.js',
+      file: 'dist/bundle/index.js',
       format: 'cjs'
     },
     {
-      file: 'dist/bundle.min.js',
+      file: 'dist/bundle/index.min.js',
       format: 'iife',
       name: 'version',
       plugins: [terser()]
