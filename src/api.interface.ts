a73db50c1a974f86061fac10a7fd3cb2acc1a425
Patrick Brisacier <pbrisacier@mncc.fr>
2021-08-06 16:19:06 +0200

    Change materializeSqlConfig type to conform with /materialize/sql API payload. Add the corresponding documentation.

--- src/api.interface.ts
+++ src/api.interface.ts
@@ -1,6 +1,64 @@
+/**
+ * Config for materializeSql API request
+ */
 type materializeSqlConfig = {
+  /**
+   * SQL Query.
+   *
+   * Example:
+   *
+   * For this SQL query:
+   * ```sql
+   * SELECT name, count(*) As Total
+   * FROM materialized_facts
+   * GROUP BY name
+   * ```
+   * Write:
+   * ```javascript
+   * const config = {
+   *     query: 'SELECT name, count(*) As Total FROM materialized_facts GROUP BY name'
+   * }
+   * ```
+   */
   query: string,
-  format: string,
+
+  /**
+   * SQL Query parameters.
+   *
+   * Example:
+   * ```sql
+   * SELECT * FROM materialized_facts
+   * WHERE name = :type
+   * ```
+   * This SQL query contains a parameter named `:type`.
+   *
+   * You can pass the parameter value with:
+   * ```javascript
+   * const config = {
+   *     query: 'SELECT * FROM materialized_facts WHERE name = :type',
+   *     params: {
+   *         type: 'my_type'
+   *     }
+   * }
+   * ```
+   */
+  params?: {
+      [index: string]: string,
+  },
+
+  /**
+   * By default, materializeSql returns data in `objects` format, an array of JSON objects.
+   * Each object is the SQL query row result.
+   *
+   * Others possibles formats are :
+   *
+   * - `arrays` : each row is returned as an array.
+   * - `csv` : each row is returned as a string where columns are comma separated (`,`).
+   * - `arrays_with_header` : same as `arrays` but the first array contains column headers.
+   * - `csv_with_header` : same as `csv` but the first string contains comma separated column headers (`,`).
+   *
+   */
+  format?: 'objects' | 'arrays' | 'arrays_with_header' | 'csv' | 'csv_with_header',
 }
 
 interface CfApiInterface {
