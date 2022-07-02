4d6eef631aed54a3e311d325833a14fb845e11fa
csavelief <csavelief@computablefacts.com>
2022-07-02 19:05:09 +0200

    PoC - Add generator for JSDoc

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import {nodeResolve} from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import compiler from '@ampproject/rollup-plugin-closure-compiler';
+import jsdoc from 'rollup-plugin-jsdoc';
 
 export default [{
   input: 'src/main.js', output: {
@@ -12,9 +13,10 @@ export default [{
     sourcemap: true,
   }, plugins: [nodeResolve(), babel({
     babelHelpers: "bundled",
+  }), jsdoc({
+    config: 'jsdoc.config.json',
   }), compiler({
-    language_out: 'STABLE',
-    warning_level: 'VERBOSE',
+    language_out: 'STABLE', warning_level: 'VERBOSE',
   })]
 }, {
   input: 'src/main.js', output: [{
